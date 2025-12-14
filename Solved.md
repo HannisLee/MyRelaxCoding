1e4d0f9e6a95c4f9a20a30088c66fee33ae5e5c2
MikeLee31 <253514942@qq.com>
2025-12-14 23:02:48 +0800

    3606

--- Solved.md
+++ Solved.md
@@ -1,3 +1,52 @@
+# 3606.[优惠券校验器](https://leetcode.cn/problems/coupon-code-validator/)
+
+```
+class Solution {
+public:
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        vector<string> validCodes;
+        vector<pair<int, string>> temp; // (priority, code)
+
+        unordered_map<string, int> prio = {
+            {"electronics", 0},
+            {"grocery", 1},
+            {"pharmacy", 2},
+            {"restaurant", 3}
+        };
+
+        for (int i = 0; i < code.size(); i++) {
+            if (code[i].empty()) continue;
+
+            bool valid = true;
+            for (char c : code[i]) {
+                if (!isalnum(c) && c != '_') {
+                    valid = false;
+                    break;
+                }
+            }
+            if (!valid) continue;
+
+            if (prio.count(businessLine[i]) == 0) continue;
+            if (!isActive[i]) continue;
+
+            temp.push_back({prio[businessLine[i]], code[i]});
+        }
+
+        sort(temp.begin(), temp.end(), [](const auto& a, const auto& b) {
+            if (a.first != b.first) 
+                return a.first < b.first; // 按业务线优先级
+            return a.second < b.second;   // 同业务线下按字典序
+        });
+
+        vector<string> res;
+        for (auto& p : temp) res.push_back(p.second);
+        return res;
+    }
+};
+```
+
+
+
 # 290.[单词规律](https://leetcode.cn/problems/word-pattern/)
 
 ```c++
--- main.cpp
+++ main.cpp
@@ -3,53 +3,54 @@ using namespace std;
 
 class Solution {
 public:
-    bool wordPattern(string pattern, string s) {
-        unordered_map<char, string> mp1; // 用来存储字符到单词的映射
-        unordered_map<string, char> mp2; // 用来存储单词到字符的映射
-        int idx = 0;
-        int last = 0;
-        s.push_back(' '); // 给字符串末尾加一个空格，方便处理最后一个单词
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        vector<string> validCodes;
+        vector<pair<int, string>> temp; // (priority, code)
 
-        for (int i = 0; i < s.size(); i++) {
-            if (isalpha(s[i])) {
-                continue;
-            }
-            else {
-                string temp = s.substr(last, i - last); // 提取当前的单词
-                if (mp1.count(pattern[idx])) {
-                    if (mp1[pattern[idx]] != temp) {
-                        return false; // 如果字符对应的单词不匹配，返回 false
-                    }
-                } else {
-                    mp1[pattern[idx]] = temp; // 正向映射
+        unordered_map<string, int> prio = {
+            {"electronics", 0},
+            {"grocery", 1},
+            {"pharmacy", 2},
+            {"restaurant", 3}
+        };
+
+        for (int i = 0; i < code.size(); i++) {
+            if (code[i].empty()) continue;
+
+            bool valid = true;
+            for (char c : code[i]) {
+                if (!isalnum(c) && c != '_') {
+                    valid = false;
+                    break;
                 }
-
-                // 反向映射检查
-                if (mp2.count(temp)) {
-                    if (mp2[temp] != pattern[idx]) {
-                        return false; // 如果单词对应的字符不匹配，返回 false
-                    }
-                } else {
-                    mp2[temp] = pattern[idx]; // 反向映射
-                }
-
-                last = i + 1; // 更新下一个单词的起始位置
-                idx++; // 移动到下一个字符
             }
+            if (!valid) continue;
+
+            if (prio.count(businessLine[i]) == 0) continue;
+            if (!isActive[i]) continue;
+
+            temp.push_back({prio[businessLine[i]], code[i]});
         }
 
-        // 如果处理完所有单词后，idx 与 pattern 的长度一致，则返回 true
-        return idx == pattern.size();
+        sort(temp.begin(), temp.end(), [](const auto& a, const auto& b) {
+            if (a.first != b.first)
+                return a.first < b.first; // 按业务线优先级
+            return a.second < b.second;   // 同业务线下按字典序
+        });
+
+        vector<string> res;
+        for (auto& p : temp) res.push_back(p.second);
+        return res;
     }
 };
-
-
 
 int main() {
     Solution s;
 
-    vector<vector<int>> gg={{1,3},{3,2},{3,3},{3,5},{5,3}};
-    cout<<s.wordPattern("abba","dog dog dog dog")<<endl;
+    vector<string> vec1={"GROCERY15","ELECTRONICS_50","DISCOUNT10"};
+    vector<string> vec2={"grocery","electronics","invalid"};
+    vector<bool> vec3 = {false,true,true};
+    cout<<s.validateCoupons(vec1,vec2,vec3)<<endl;
 
 
 
